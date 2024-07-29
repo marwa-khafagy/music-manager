64d34e1e7254a0197b5b4e4c30fa5e9eace4b547
marwa-khafagy <99355272+marwa-khafagy@users.noreply.github.com>
2024-07-28 22:20:49 -0400

    refactored tut code to seperate main from requests

---
+++ playlist_names.py
@@ -0,0 +1,25 @@
+import requests
+from flask import jsonify, redirect
+from datetime import datetime
+import os
+
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+API_BASE_URL = os.getenv('API_BASE_URL')
+
+def get_playlists(session):
+    if 'access_token' not in session:
+        return redirect('/login')
+
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh-token')
+
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}"
+    }
+
+    response = requests.get(f"{API_BASE_URL}me/playlists", headers=headers)
+    playlists = response.json()
+
+    return jsonify(playlists)
--- tut.py
+++ tut.py
@@ -1,14 +1,23 @@
-from flask import Flask, redirect, request, session, jsonify
-from datetime import datetime
-import os
-import urllib.parse
+# 
 import requests
 
-# from playlist_names import get_playlists
-import playlist_names
+from datetime import datetime, timedelta
+from flask import Flask , redirect, request, session, jsonify
 
-# Load environment variables
 from dotenv import load_dotenv
+import os
+
+import urllib.parse
+
+# what is flask seceret key?
+# The Flask secret key is a cryptographic key 
+# used to encrypt session cookies and other 
+# secure data in Flask applications. It is used 
+# to provide security and prevent tampering of data.
+app = Flask(__name__)
+app.secret_key = "fvY2WQ8S-8t9UZ_53BRkrQ"
+
+# To get the global variables from the .env file
 load_dotenv()
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
@@ -17,9 +26,7 @@ AUTH_URL = os.getenv('AUTH_URL')
 TOKEN_URL = os.getenv('TOKEN_URL')
 API_BASE_URL = os.getenv('API_BASE_URL')
 
-app = Flask(__name__)
-app.secret_key = "fvY2WQ8S-8t9UZ_53BRkrQ"
-
+# root of the flask app
 @app.route('/')
 def index():
     return "Welcome welcome <a href='/login'>Login</a>"
@@ -33,13 +40,14 @@ def login():
         'response_type': 'code',
         'redirect_uri': REDIRECT_URL,
         'scope': scope, 
+        # by defualt it is False
+        # setting it to true will make it so that you have to login every time
         'show_dialog': True
     }
 
-    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+    auth_url = f"{AUTH_URL}?{(urllib.parse.urlencode(params))}"
 
     return redirect(auth_url)
-
 
 @app.route('/callback')
 def callback():
@@ -53,29 +61,46 @@ def callback():
             'redirect_uri': REDIRECT_URL,
             'client_id': CLIENT_ID,
             'client_secret': CLIENT_SECRET
+
         }
 
         response = requests.post(TOKEN_URL, data=req_body)
         token_info = response.json()
 
+        # to make requests to the api
         session['access_token'] = token_info['access_token']
+        # to refresh the token
         session['refresh_token'] = token_info['refresh_token']
-        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
+        # to get the time the token expires
+        session['expires_at']  = datetime.now().timestamp() + token_info['expires_in']
     
+    # redirect 
     return redirect('/playlists')
 
-
 @app.route('/playlists')
-def playlists():
-    return playlist_names.get_playlists(session)
+def get_playlists():
+    if 'access_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh')
+    
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}"
+    }
 
+    response = requests.get(f"{API_BASE_URL}me/playlists", headers=headers)
+    playlists = response.json()
+
+    return jsonify(playlists)
 
 @app.route('/refresh-token')
 def refresh_token():
     if 'refresh_token' not in session:
         return redirect('/login')
-
+    
     if datetime.now().timestamp() > session['expires_at']:
+        
         req_body = {
             'grant_type': 'refresh_token',
             'refresh_token': session['refresh_token'],
@@ -94,3 +119,4 @@ def refresh_token():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
