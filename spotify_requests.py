1d51781809453dc478bd5c2e965e5fe4d404450b
marwa-khafagy <99355272+marwa-khafagy@users.noreply.github.com>
2024-08-24 14:27:55 -0400

    add comments

--- spotify_requests.py
+++ spotify_requests.py
@@ -1,8 +1,11 @@
 import json
 import requests
 from flask import jsonify, redirect, render_template
+# for refresh token 
 from datetime import datetime
+
 import global_functions
+# for environment variables
 import os
 from dotenv import load_dotenv
 
@@ -13,6 +16,8 @@ AR_GENRES = os.getenv('ARAB_GENRES')
 PLAYLIST = os.getenv('PLAYLIST')
 PLAYLIST_ID = os.getenv('PLAYLIST_ID')
 
+# input 'playlists' to print all available playlists
+# can be used to get playlist id
 def get_playlists(session):
     if 'access_token' not in session:
         return redirect('/login')
@@ -50,6 +55,9 @@ def get_liked_tracks(session, offset):
 
     return tracks
 
+# prints all arabic liked tracks in terminal
+# prints all ar songs not in the playlist in web 
+# input 'recent_ar_tracks' to get run this
 def get_recent_ar_tracks(session):
     if 'access_token' not in session:
         return redirect('/login')
@@ -57,29 +65,41 @@ def get_recent_ar_tracks(session):
     if datetime.now().timestamp() > session['expires_at']:
         return redirect('/refresh-token')
 
+    # fetches last 100 liked tracks
     liked_tracks = get_liked_tracks(session, 0) + get_liked_tracks(session, 50)
+    
+    # filters liked tracks to arabic tracks 
     ar_tracks = [track for track in liked_tracks if is_ar_track(session, track['track'])]
 
+    # fetches all tracks in the ar playlist
     playlist = get_ar_playlist(session)
 
+    # gets all track ids in the playlist
     playlist_track_ids = {track['track']['id'] for track in playlist if track['track']}
 
-    unique_ar_tracks = [track for track in ar_tracks if track['track']['id'] not in playlist_track_ids]
-
-    print((len(unique_ar_tracks)), len(ar_tracks), len(playlist_track_ids), len(playlist))
-    
-    for idx, track in enumerate(ar_tracks):
-        if track['track']['name']:
-            print(f"{idx + 1}. {track['track']['name']}")
+    # filters out and prints tracks that are in the playlist
+    unique_ar_tracks = []
+    idx = 1
+    print("Arabic liked tracks already in the playlist:")
+    for track in ar_tracks:
+        if track['track']['id'] not in playlist_track_ids:
+            unique_ar_tracks.append(track)
+        else:
+            print(f"{idx + 1}.{track['track']['name']}")
+            idx += 1
 
     return render_template('liked.html', tracks=unique_ar_tracks)
 
+# checks if the track is arabic
 def is_ar_track(session, track):
     artist_id = track['artists'][0]['id']
     if artist_id is None:
         return False
 
+    # list of genres associated with the artist
     genres = get_genre_by_artist(session, artist_id)
+
+    # checks if any of the genres are arabic
     for genre in genres:
         genre_words = genre.split()
         for word in genre_words:
@@ -88,12 +108,15 @@ def is_ar_track(session, track):
     
     return False
 
+# returns an artist's id given their name
+# not used in the final code
 def get_artist_id(session, artist_name):
     url = f'{API_BASE_URL}search'
     headers = global_functions.get_auth_header(session['access_token'])
     query = f"?q={artist_name}&type=artist&limit=1"
     query_url = url + query
 
+    # try to get artist
     try:
         response = requests.get(query_url, headers=headers)
         response.raise_for_status()
@@ -101,6 +124,7 @@ def get_artist_id(session, artist_name):
         print("Failed to fetch artist:", e)
         return jsonify({'error': 'Failed to fetch artist'}), 500
 
+    # parses the possible artists
     try:
         artists = response.json()["artists"]["items"]
     except (json.JSONDecodeError, KeyError) as e:
@@ -110,13 +134,16 @@ def get_artist_id(session, artist_name):
     if not artists:
         print(f"Artist {artist_name} not found")
         return None
-
+    
+    # returns the first artist's id
     return artists[0]["id"]
 
+# checks the genres that the artist is associated with
 def get_genre_by_artist(session, artist_id):
     url = f"{API_BASE_URL}artists/{artist_id}"
     headers = global_functions.get_auth_header(session['access_token'])
 
+    # try to get artist genres
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
@@ -124,6 +151,7 @@ def get_genre_by_artist(session, artist_id):
         print("Failed to fetch artist genres:", e)
         return jsonify({'error': 'Failed to fetch artist genres'}), 500
 
+    # parses the genres
     try:
         genres = response.json().get("genres", [])
     except (json.JSONDecodeError, KeyError) as e:
@@ -132,6 +160,7 @@ def get_genre_by_artist(session, artist_id):
 
     return genres
 
+# returns all the track id's in the playlist
 def get_ar_playlist(session):
     if 'access_token' not in session:
         return redirect('/login')
@@ -140,10 +169,15 @@ def get_ar_playlist(session):
         return redirect('/refresh-token')
 
     headers = global_functions.get_auth_header(session['access_token'])
+
+    # fetches the number of songs in the playlist
     num_of_songs = requests.get(f"https://api.spotify.com/v1/playlists/{PLAYLIST_ID}/tracks?fields=total", headers=headers)
     num_of_songs = num_of_songs.json()['total']
+    
     offset = 0
     songs = []
+
+    # fetches all the songs in the playlist, by 100 songs at a time
     while offset < num_of_songs:
         response = requests.get(f"https://api.spotify.com/v1/playlists/{PLAYLIST_ID}/tracks?fields=items.track.id&limit=100&offset={offset}", headers=headers)
         if response.status_code != 200:
@@ -152,6 +186,7 @@ def get_ar_playlist(session):
         playlist = response.json()
         songs.extend(playlist['items'])
         offset += 100
+    
     return songs
 
 
